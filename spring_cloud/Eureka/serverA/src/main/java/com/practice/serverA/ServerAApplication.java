45d6f6ae5fd6805070a9bf513f949522e5dc5528
jyh4479 <jyh.dev@gmail.com>
2021-02-27 16:11:29 +0900

    prepare eureka test

---
+++ spring_cloud/Eureka/serverA/src/main/java/com/practice/serverA/ServerAApplication.java
@@ -0,0 +1,13 @@
+package com.practice.serverA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerAApplication.class, args);
+	}
+
+}
