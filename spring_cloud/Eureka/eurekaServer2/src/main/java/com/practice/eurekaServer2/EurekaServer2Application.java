7aa992a6935b215fa3d8782af7027695015fdc5f
jyh4479 <jyh.dev@gmail.com>
2021-02-28 18:28:36 +0900

    Add Eureka Server

---
+++ spring_cloud/Eureka/eurekaServer2/src/main/java/com/practice/eurekaServer2/EurekaServer2Application.java
@@ -0,0 +1,13 @@
+package com.practice.eurekaServer2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServer2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServer2Application.class, args);
+	}
+
+}
